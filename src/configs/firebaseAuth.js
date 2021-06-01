c50823305a801f6fd9f5fc6ad4ea8a5f00614416
Luan Nguyen <design.luannguyen@gmail.com>
2021-06-01 17:01:59 +0700

    Initial commit

---
+++ src/configs/firebaseAuth.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCj376WDKhFfXiCQ1XZw_lm- oArYhP0ZLQ",
+  authDomain: "react-native-for-designers.firebaseapp.com",
+  databaseURL: "https://react-native-for-designers.firebaseio.com",
+  // storageBucket: "react-native-for-designers.appspot.com",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
