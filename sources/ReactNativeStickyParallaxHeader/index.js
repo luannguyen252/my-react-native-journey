196a17e31e1e87f63a3a382f3e39668e1ab53c86
Luan Nguyen <design.luannguyen@gmail.com>
2021-06-11 13:23:10 +0700

    React Native

---
+++ sources/ReactNativeStickyParallaxHeader/index.js
@@ -0,0 +1,13 @@
+// https://github.com/netguru/sticky-parallax-header
+import React from "react";
+import StickyParallaxHeader from "react-native-sticky-parallax-header";
+
+const ReactNativeStickyParallaxHeader = () => (
+  <>
+    <StickyParallaxHeader headerType="TabbedHeader" />
+    {/* <StickyParallaxHeader headerType="AvatarHeader" /> */}
+    {/* <StickyParallaxHeader headerType="DetailsHeader" /> */}
+  </>
+);
+
+export default ReactNativeStickyParallaxHeader;
